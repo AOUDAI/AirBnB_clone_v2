f0212c94ffb84445ff495d10ce8ec77510bf41bb
AOUDAI <aoudair16031999@gmail.com>
2024-02-24 00:49:34 +0100

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,6 +18,8 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls:
+            if isinstance(cls, str):
+                cls = eval(cls)
             instances = {}
             for key, value in self.__objects.items():
                 if isinstance(value, cls):
