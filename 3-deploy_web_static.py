2c3107ac60e4ab93a1438366b5dd19b2a9ac9876
AOUDAI <aoudair16031999@gmail.com>
2024-03-11 17:42:58 +0100

    test

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import os.path
 from fabric.api import put, run, env, local
 
-env.hosts = ['35.243.128.200', '3.239.120.96']
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_pack():
