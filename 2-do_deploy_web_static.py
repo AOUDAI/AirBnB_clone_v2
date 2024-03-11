2eb54aaceaa7ccf1b9e61cfe1212aba0b1fcd522
AOUDAI <aoudair16031999@gmail.com>
2024-03-11 12:18:49 +0100

    add task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Defines do_deploy function"""
+
+from fabri.api import put, env, run
+    
+def do_deploy(archive_path):
+    """Distributes an archive file into a server"""
+
+    fileName = (archive_path.split('.')[0]).split('/')[1]
+    archiveFile = f"/tmp/{archive_path.split('/')[1]}"
+    dataPath = f"/data/web_static/releases/{fileName}/"
+    linkPath = '/data/web_static/current'
+
+    try:
+        put(archive_path, '/tmp/')
+        run(f"mkdir -p {dataPath}")
+        run(f"tar -xzvf {archiveFile} -C {dataPath} --strip-components=1")
+        run(f"rm {archiveFile}")
+        run(f"rm {linkPath}")
+        run(f"ln -s {dataPath} {linkPath} ")
+
+    except Exception:
+        return False
+    
+    return True
+    
--- fabfile.py
+++ fabfile.py
@@ -1,2 +1,36 @@
-def hello():
-    print("Hello world!")
+from fabric import task, Connection
+import time
+
+@task
+def do_pack(c):
+    """Creates an archive file for web_static directory"""
+
+    try:
+        c.run("mkdir -p versions")
+        archiveTime = time.strftime("%Y%m%d%H%M%S")
+        c.run(f"tar -czvf versions/web_static_{archiveTime}.tgz web_static/")
+        return archiveTime
+    except Exception:
+        return None
+    
+@task
+def do_deploy(c, archive_path):
+    """Distributes an archive file into a server"""
+
+    fileName = (archive_path.split('.')[0]).split('/')[1]
+    archiveFile = f"/tmp/{archive_path.split('/')[1]}"
+    dataPath = f"/data/web_static/releases/{fileName}/"
+    linkPath = '/data/web_static/current'
+
+    try:
+        c.put(archive_path, '/tmp/')
+        c.run(f"mkdir -p {dataPath}")
+        c.run(f"tar -xzvf {archiveFile} -C {dataPath} --strip-components=1")
+        c.run(f"rm {archiveFile}")
+        c.run(f"rm {linkPath}")
+        c.run(f"ln -s {dataPath} {linkPath} ")
+
+    except Exception:
+        return False
+    
+    return True
