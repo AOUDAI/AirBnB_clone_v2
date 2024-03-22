56539fa903b5ac613d5d708588277f8bac98bc4f
AOUDAI <aoudair16031999@gmail.com>
2024-03-22 23:50:07 +0100

    add task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Contains a simple flask web application"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def for_c(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/<text>')
+@app.route('/python')
+def for_python(text='is cool'):
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>')
+def is_number(n):
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>')
+def template_renderer(n):
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
