6ec4e9faa923f682f5d258f26839c1198b24b333
AOUDAI <aoudair16031999@gmail.com>
2024-04-18 00:45:14 +0100

    add task 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Flask web application that fetchs data from storage"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+
+@app.route('/cities_by_states')
+def States():
+    states = list(storage.all(State).values())
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def clearUp(exeption):
+    """ clean things up after the app is completed"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
